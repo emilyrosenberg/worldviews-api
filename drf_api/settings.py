c797f991a16cb01af78ede9d9250d84a0ceaf6d3
Emily Rosenberg <earosenberg@gmail.com>
2024-02-24 12:39:15 +0000

    More deployment fixes 2

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -83,9 +83,9 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
 
 # from PP5 slack channel
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_HEADERS = list(default_headers)
-CORS_ALLOW_METHODS = list(default_methods)
-CSRF_TRUSTED_ORIGINS = [os.environ.get('CLIENT_ORIGIN_DEV', 'CLIENT_ORIGIN',)]
+# CORS_ALLOW_HEADERS = list(default_headers)
+# CORS_ALLOW_METHODS = list(default_methods)
+# CSRF_TRUSTED_ORIGINS = [os.environ.get('CLIENT_ORIGIN_DEV', 'CLIENT_ORIGIN',)]
 
 # Application definition
 
