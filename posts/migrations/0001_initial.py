bddea3e8773606650a26a02bc6e911fdefe0e7d6
Emily Rosenberg <earosenberg@gmail.com>
2024-02-26 12:34:20 +0000

    Update link for default profile image

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
                 ('updated_at', models.DateTimeField(auto_now=True)),
                 ('title', models.CharField(max_length=255)),
                 ('content', models.TextField(blank=True)),
-                ('image', models.ImageField(blank=True, default='../default_post_rgq6aq', upload_to='images/')),
+                ('image', models.ImageField(blank=True, default='../default_post_vr6ucz', upload_to='images/')),
                 ('owner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
