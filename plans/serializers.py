11e6f836ca95e03a0c45e9996ca3d020488f67f0
Emily Rosenberg <earosenberg@gmail.com>
2024-03-06 19:24:20 +0000

    Fix typo

--- plans/serializers.py
+++ plans/serializers.py
@@ -18,5 +18,5 @@ class PlanSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'plan_comments_count', 'locations',
+            'title', 'content', 'plan_comments_count', 'location',
         ]
